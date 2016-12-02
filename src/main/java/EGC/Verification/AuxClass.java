cfd935d550d32acfe91e17cd24339361b4796f29
AlvaroSL <varosl94@gmail.com>
2016-12-02 16:59:32 +0100

    Eliminadas las comunicaciones con la base de datos y los métodos que implementaban las mismas

--- src/main/java/EGC/Verification/AuxClass.java
+++ src/main/java/EGC/Verification/AuxClass.java
@@ -7,8 +7,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -16,8 +14,6 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 
 public class AuxClass {
 	
@@ -52,19 +48,6 @@ public class AuxClass {
 		 return resumen;
 	}
 	
-
-	
-	//funcion para generar clave DES y almacenarla en base de datos
-	// guarda el string de la secret key en base de datos
-	public static void postKeyDES(String id) throws NoSuchAlgorithmException{
-		KeyGenerator generadorDES = KeyGenerator.getInstance("DES");
-		generadorDES.init(56); // clave de 56 bits
-		SecretKey clave = generadorDES.generateKey();
-		String stringclave = Base64.getEncoder().encodeToString(clave.getEncoded()); //convierte la key a string
-		DataBaseManager dbm=new DataBaseManager(); // para utulizar metodos de esta clase
-		dbm.saveKeyDES(id,stringclave);
-		
-	}
 	
 	//genera una clave para DES y la devuelve
 	public static SecretKey returnKeyDes(){
@@ -93,17 +76,6 @@ public class AuxClass {
 	}
 	
 	
-	//recuperar clave "string" de la base de datos y devolverla como SecretKey
-	public static SecretKey getKeyDES(String id){
-		
-		DataBaseManager dbm=new DataBaseManager(); // para utulizar metodos de esta clase
-		String secretkeystring = dbm.getKeyDES(id);
-		//decodificamos de base64
-		byte[] decodedKey = Base64.getDecoder().decode(secretkeystring);
-		//reconstruimos
-		SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "DES"); 
-		return originalKey;
-	}
 	// cifrando en des, se le debe pasar el resumen(de una funcion resumen como MD5)
 	//como text
 	public static byte[] encryptDES(SecretKey key, String text) 
@@ -177,55 +149,6 @@ public class AuxClass {
 
 		
 		return res;
-	}
-	
-	//metodos auxiliares (RSA):
-	public static boolean postKeyRSA(String id) {
-		boolean success = false;
-		try {
-
-			
-			 KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-			
-			 SecureRandom random = SecureRandom.getInstance("SHA1PRNG","SUN");
-			 keyGen.initialize(2048, random);
-			
-			 KeyPair pair = keyGen.generateKeyPair();
-			
-			 
-			 String publicKey = DatatypeConverter.printBase64Binary(pair.getPublic().getEncoded());
-			 String privateKey= DatatypeConverter.printBase64Binary(pair.getPrivate().getEncoded());
-			 
-			 
-			 RemoteDataBaseManager rdbm=new RemoteDataBaseManager();
-			 if (rdbm.postKeys(id, publicKey, privateKey)){
-				 success = true;
-			 }
-			
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	
-		
-		return success;
-	}
-
-	
-	public static String getPublicKeyRSA(String id) {
-		RemoteDataBaseManager rdbm=new RemoteDataBaseManager();
-		
-		return rdbm.getPublicKey(id);
-	}
-
-	
-	public static String getPrivateKeyRSA(String id) {
-
-		RemoteDataBaseManager rdbm=new RemoteDataBaseManager();
-		
-		
-		return rdbm.getPrivateKey(id);
 	}
 
 	
